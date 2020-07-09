9734373530226f44dc343606efd306feb2f25af2
BelkaDev <belkacem.hadjoudj@esprit.tn>
2020-07-09 07:05:48 +0100

    code improvement

--- examples/ApiCall.js
+++ examples/ApiCall.js
@@ -1,17 +1,14 @@
-const 
-  tokenValidator = require('../lib/tokenValidator')
+const tokenValidator = require('../lib/tokenValidator')
 const 
   BASE_URL = 'api.spotify.com',
   USER_AGENT="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0";
 
 async function getToken(session) {
 await session.checkTokenValidity();
-if (session.isAlive) {
-return session.accessToken;
-} else {
+if (!session.isAlive) {
 await session.generate()
-return session.accessToken;
 }
+return session.accessToken;
 }
 
 async function main() {
--- index.js
+++ index.js
@@ -6,13 +6,10 @@ async function main() {
   await session.checkTokenValidity();
   if (session.isAlive) {
   console.log('You have an ongoing valid session, skipping the logging process.');
-  console.log('ACCESS_TOKEN: '+session.accessToken);
-  return;
 } else
 {
   console.log('Previous token expired or not retrieved, generating a new one.')
   await session.generate()
-  console.log('\nACCESS_TOKEN:'+session.accessToken);
-  (session.accessToken) ?  console.log('\nSuccess. (Exiting)') : console.log('\nError while getting the token.')
 }
+console.log('\nACCESS_TOKEN:'+session.accessToken);
 }
