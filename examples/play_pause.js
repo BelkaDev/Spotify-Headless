5d07981370227a1ad113655a606da2f3c1e5ee37
BelkaDev <belkacem.hadjoudj@esprit.tn>
2020-07-09 12:24:40 +0100

    sending actions (play,pause)

--- examples/play_pause.js
+++ examples/play_pause.js
@@ -1,4 +1,5 @@
 const tokenValidator = require('../lib/tokenValidator')
+const client = require('./playerStatus')
 const 
   BASE_URL = 'api.spotify.com',
   USER_AGENT="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0";
@@ -14,8 +15,8 @@ return session.accessToken;
 async function main() {
 const session = new tokenValidator();
 const token = await getToken(session);
-result = await session.sendRequest('/v1/me/player/devices')
-console.log(result);
+const player = new client(session);
+var x = await player.sendAction('/connect-state/v1/player/command/from/22d6aea8bbb189ebd4616c8655a473ba4e513d85/to/140a29100980b698f8d97e74145c87be8d1bcd42');
 }
 
 main()
---
+++ examples/playerStatus.js
@@ -0,0 +1,52 @@
+var request = require('request');
+const tokenValidator = require('../lib/tokenValidator')
+const 
+  BASE_URL = 'gew-spclient.spotify.com',
+  USER_AGENT="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0";
+
+class player {
+    constructor(session) {
+    	this.currentDevice=null;
+      this.isPlaying=false;
+    	this.auth=session.accessToken;
+         }
+
+  async sendAction(apiCall) {
+  	var headers = {
+    'Authorization': `Bearer ${this.auth}`,
+    'authority': BASE_URL,
+    'User-Agent': USER_AGENT,
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'content-type': 'text/plain;charset=UTF-8',
+    'accept': '*/*',
+};
+var action=""
+await this.getStatus();
+this.isPlaying ? action = 'pause' : action = 'resume'
+var command = '{"command":{"endpoint":"'+action+'"}}';
+var options = {
+    url: 'https://'+BASE_URL+'/connect-state/v1/player/command/from/source/to/'+this.currentDevice,
+    method: 'POST',
+    headers: headers,
+    body: command
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+    this.isPlaying ? console.log('pausing') : console.log('resuming')
+    }
+}
+
+request(options, callback.bind(this));
+  }
+
+async getStatus () {
+const session = new tokenValidator();
+const result = await session.sendRequest('/v1/me/player/')
+this.currentDevice=result.device.id;
+this.isPlaying=result.is_playing;
+}
+}
+module.exports = player;
+
